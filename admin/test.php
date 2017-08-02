f46a70212a3913d4f2596f48c966a1d5cc08e685
Ben Beach <subz33r0@gmail.com>
2017-08-02 13:47:59 -0400

    Remove 304/305

--- admin/test.php
+++ admin/test.php
@@ -7,11 +7,11 @@ $rooms_qry = $dbconn->query("SELECT * FROM rooms WHERE individual_bracket_buildo
 
 if($rooms_qry->num_rows > 0) {
     while($rooms = $rooms_qry->fetch_assoc()) {
-        $first_slice = explode("86,87,88,19", $rooms['individual_bracket_buildout']);
+        $first_slice = explode("111,112", $rooms['individual_bracket_buildout']);
 
-        $middle_slice = "110,111,112,113,114,115,19";
+        //$middle_slice = "110,111,112,113,114,115,19";
 
-        $final_slice = $first_slice[0] . $middle_slice . $first_slice[1];
+        $final_slice = $first_slice[0] . $first_slice[1];
 
         $dbconn->query("UPDATE rooms SET individual_bracket_buildout = '$final_slice' WHERE id = '{$rooms['id']}'");
     }
