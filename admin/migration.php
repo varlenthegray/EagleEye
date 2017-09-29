75cf1aeb0e7db51d4dd0a74c56d1a02549ab4494
Ben Beach <subz33r0@gmail.com>
2017-09-29 09:21:12 -0400

    Migration file updated to not be so hard on the SQL DB.

--- admin/migration.php
+++ admin/migration.php
@@ -1,7 +1,7 @@
 <?php
 require ("../includes/header_start.php");
 
-$start_qry = $dbconn->query("SELECT * FROM op_audit_trail");
+$start_qry = $dbconn->query("SELECT * FROM op_audit_trail WHERE start_time IS NULL");
 
 while($start = $start_qry->fetch_assoc()) {
     $changed = json_decode($start['changed'], true);
@@ -10,7 +10,7 @@ while($start = $start_qry->fetch_assoc()) {
         $dbconn->query("UPDATE op_audit_trail SET start_time = '{$changed['Start Time']}' WHERE id = '{$start['id']}'");
 }
 
-$resumed_qry = $dbconn->query("SELECT * FROM op_audit_trail");
+$resumed_qry = $dbconn->query("SELECT * FROM op_audit_trail WHERE start_time IS NULL");
 
 while($resumed = $resumed_qry->fetch_assoc()) {
     $changed = json_decode($resumed['changed'], true);
@@ -19,7 +19,7 @@ while($resumed = $resumed_qry->fetch_assoc()) {
         $dbconn->query("UPDATE op_audit_trail SET start_time = '{$changed['Resumed Time']}' WHERE id = '{$resumed['id']}'");
 }
 
-$ended_qry = $dbconn->query("SELECT * FROM op_audit_trail");
+$ended_qry = $dbconn->query("SELECT * FROM op_audit_trail WHERE end_time IS NULL");
 
 while($ended = $ended_qry->fetch_assoc()) {
     $changed = json_decode($ended['changed'], true);
