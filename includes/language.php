e3270a3c7d9ed84c114702a78c8c072deb060ad4
Ben Beach <subz33r0@gmail.com>
2017-11-14 08:05:09 -0500

    CV operation not working.

--- includes/language.php
+++ includes/language.php
@@ -3,7 +3,7 @@
  * To define the variable language inside of the system
  */
 // Release Date
-define("RELEASE_DATE", "11/10/17");
+define("RELEASE_DATE", "11/13/17");
 
 // Global definitions
 define("LOGO_TEXT", "Stone Mountain Cabinetry"); // The logo text displayed
