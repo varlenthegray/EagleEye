222adaa503f5ed57be74fa1f4f78b146981dd8b9
Ben Beach <subz33r0@gmail.com>
2017-11-21 16:27:52 -0500

    Increment version

--- includes/language.php
+++ includes/language.php
@@ -3,7 +3,7 @@
  * To define the variable language inside of the system
  */
 // Release Date
-define("RELEASE_DATE", "11/20/17");
+define("RELEASE_DATE", "11/21/17");
 
 // Global definitions
 define("LOGO_TEXT", "Stone Mountain Cabinetry"); // The logo text displayed
