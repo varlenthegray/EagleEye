e9601fdfdcc1237b81b04909eeffa50c1f371188
Ben Beach <subz33r0@gmail.com>
2018-04-11 13:36:50 -0400

    Large amount of pricing program updates including:   Coversheet now in UI   Find now incorporated (but not functional)   Menu on left now uses fancytree rather than manual code

--- pricing/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require '../includes/header_start.php';
-require '../includes/header_end.php';
-?>
-
-<?php 
-require '../includes/footer_start.php';
-require '../includes/footer_end.php'; 
-?>
