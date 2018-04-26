a922f2e5cb2e47de3bac01fe7dacaa9be3abef6c
Ben Beach <subz33r0@gmail.com>
2018-04-26 16:36:08 -0400

    Adjusted and began the process of an all in one solution

--- html/opl/all.php
+++ html/opl/all.php
@@ -6,15 +6,16 @@ outputPHPErrs();
 $output = array();
 $i = 0;
 
-$opl_qry = $dbconn->query('SELECT ou.opl, u.name FROM opl_users ou LEFT JOIN user u on ou.user_id = u.id;');
+$opl_qry = $dbconn->query('SELECT ou.opl, u.name, u.id FROM opl_users ou LEFT JOIN user u on ou.user_id = u.id;');
 
 while($opl = $opl_qry->fetch_assoc()) {
   $output[$i]['title'] = $opl['name'];
-  $output[$i]['children'] = $opl['opl'];
+  $output[$i]['lazy'] = true;
   $output[$i]['folder'] = true;
+  $output[$i]['user_id'] = $opl['id'];
 
   $i++;
-
-  echo $opl['opl'];
 }
+
+echo json_encode($output, true);
 
