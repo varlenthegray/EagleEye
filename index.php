764ebb94ce28064681a1f06f559d2a5ebd078f04
Ben Beach <subz33r0@gmail.com>
2018-06-07 13:23:57 -0400

    Updated images to not have dimensions Began process of working on custom select box

--- index.php
+++ index.php
@@ -57,6 +57,9 @@ require 'includes/header_start.php';
     <!-- Alert Windows -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-confirm/3.3.0/jquery-confirm.min.css">
 
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js"></script>
+
     <?php
     $server = explode(".", $_SERVER['HTTP_HOST']);
 
