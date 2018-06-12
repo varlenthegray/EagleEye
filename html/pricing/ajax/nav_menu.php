a9888eedd98cb77fd705acbdf0a00cf84821a509
Ben Beach <subz33r0@gmail.com>
2018-06-12 11:12:22 -0400

    Updated lots of images, almost done with nav missing category bug

--- html/pricing/ajax/nav_menu.php
+++ html/pricing/ajax/nav_menu.php
@@ -3,17 +3,15 @@ require '../../../includes/header_start.php';
 
 //outputPHPErrs();
 
-$catalog_id = !empty(sanitizeInput($_REQUEST['catalog'])) ? sanitizeInput($_REQUEST['catalog']) : 2;
-
 // Brandon Christensen 4/14/2018
 $result = array();
 
-$parent_qry = $dbconn->prepare('SELECT 
-  pc.id AS catID, pn.id AS itemID, name, parent, sort_order, pn.category_id, pn.sku, detail.image_path, pc.catalog_id
+$parent_qry = $dbconn->prepare('SELECT
+  pc.id AS catID, pn.id AS itemID, name, parent, sort_order, pn.category_id, pn.sku, detail.image_path, pc.catalog_id AS catalog_id, pn.catalog_id AS itemCatalogID
 FROM pricing_categories pc
   LEFT JOIN pricing_nomenclature pn on pc.id = pn.category_id
   LEFT JOIN pricing_nomenclature_details detail on pn.description_id = detail.id
-WHERE parent = ? AND (pn.catalog_id = 2 OR pn.catalog_id IS NULL) ORDER BY pc.catalog_id, parent, sort_order, catID ASC');
+WHERE parent = ? ORDER BY pc.catalog_id, parent, sort_order, catID ASC;');
 
 function makeTree($parent_id) {
   global $parent_qry;
@@ -21,14 +19,14 @@ function makeTree($parent_id) {
   $ret = array();
 
   $parent_qry->bind_param('i', $parent_id);
-  $parent_qry->bind_result($catID, $itemID, $name, $parent, $sort_order, $item_catID, $sku, $image, $catalog_id);
+  $parent_qry->bind_result($catID, $itemID, $name, $parent, $sort_order, $item_catID, $sku, $image, $catCatalogID, $itemCatalogID);
   $parent_qry->execute();
   $parent_qry->store_result();
 
   $data = array();
 
   while ($parent_qry->fetch()) {
-    if($catalog_id === 1) { // if it's SMC's catalog, color it green
+    if($catCatalogID === 1) { // if it's SMC's catalog, color it green
       $cat_bg = 'rgba(0,255,0,.25)';
     } else { // otherwise, it's someone else's catalog, color it orange
       $cat_bg = 'rgba(255,165,0,.25)';
@@ -43,7 +41,9 @@ function makeTree($parent_id) {
       'item_catID' => $item_catID,
       'sku' => $sku,
       'image_path' => $image,
-      'cat_bg' => $cat_bg
+      'cat_bg' => $cat_bg,
+      'catCatalogID' => $catCatalogID,
+      'itemCatalogID' => $itemCatalogID
     );
   }
 
@@ -59,15 +59,17 @@ function makeTree($parent_id) {
         $ret[] = &$sku_items[$item['item_catID']];
       }
 
-      $title = $item['sku'];
-      $title .= " <span class='actions'>
-            <div class='info_container'><i class='fa fa-info-circle primary-color view_item_info' data-id='{$item['itemID']}'></i></div>
-            <i class='fa fa-plus-circle success-color add_item_cabinet_list' data-id='{$item['itemID']}' title='Add To Cabinet List'></i>
-          </span>";
+      if($item['itemCatalogID'] === 1) {
+        $title = $item['sku'];
+        $title .= " <span class='actions'>
+          <div class='info_container'><i class='fa fa-info-circle primary-color view_item_info' data-id='{$item['itemID']}'></i></div>
+          <i class='fa fa-plus-circle success-color add_item_cabinet_list' data-id='{$item['itemID']}' title='Add To Cabinet List'></i>
+        </span>";
 
-      $img = !empty($item['image_path']) ? "/html/pricing/images/{$item['image_path']}" : 'fa fa-magic';
+        $img = !empty($item['image_path']) ? "/html/pricing/images/{$item['image_path']}" : 'fa fa-magic';
 
-      $sku_items[$item['item_catID']]['children'][] = array('key' => $item['itemID'],'icon' => $img, 'title' => $title, 'is_item' => true, 'qty' => 1);
+        $sku_items[$item['item_catID']]['children'][] = array('key' => $item['itemID'],'icon' => $img, 'title' => $title, 'is_item' => true, 'qty' => 1);
+      }
     } else {
       $object = array('key' => $item['catID'], 'folder' => true, 'title' => "<span style='background-color:{$item['cat_bg']};'>{$item['name']}</span>");
 
