e54ad2f2db328bf6aaeeef01682c096af7fe33e4
Ben Beach <subz33r0@gmail.com>
2018-06-25 07:33:06 -0400

    Launching pricing program

--- includes/language.php
+++ includes/language.php
@@ -3,8 +3,8 @@
  * To define the variable language inside of the system
  */
 // Release Date
-define('RELEASE_DATE', '6/19/18');
-define('VERSION', '2.3.671');
+define('RELEASE_DATE', '6/25/18');
+define('VERSION', '2.4.0');
 
 // Global definitions
 define('LOGO_TEXT', 'Stone Mountain Cabinetry'); // The logo text displayed
