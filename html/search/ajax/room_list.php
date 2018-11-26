8f71d9bfa2bed0a73063bec79178f7f94a9f1f58
Ben Beach <subz33r0@gmail.com>
2018-11-26 15:00:04 -0500

    Implementing new search results, bug currently with displaying columns inside of nested child in DataTables - DataTables class that is added automatically causes cols to not properly load

--- html/search/ajax/room_list.php
+++ html/search/ajax/room_list.php
@@ -7,45 +7,113 @@
  */
 require_once '../../../includes/header_start.php';
 
+$strip = (bool)$_REQUEST['strip']; // if we're trying to view in a browser with JSON data, remove HTML info
+
 //outputPHPErrs();
 
-$soID = sanitizeInput($_REQUEST['so_id']);
-$output = [];
-$operations = [];
-$i = 0;
+$vin_schema = array();
 
+// obtain the VIN database table and commit to memory for this query (MAJOR reduction in DB query count)
+$vin_qry = $dbconn->query("SELECT * FROM vin_schema ORDER BY FIELD(`value`, 'Custom/Other', 'TBD', 'N/A', 'Completed', 'Job', 'Quote', 'Lost') DESC, segment, `key` ASC");
+
+while($vin = $vin_qry->fetch_assoc()) {
+  $vin_schema[$vin['segment']][$vin['key']] = $vin['value'];
+}
+
+$soID = sanitizeInput($_REQUEST['so_id']); // get the SO ID
+$output = []; // final output
+$operations = []; // operation information
+$i = 0; // increment for displaying in order
+
+// get all operations
 $op_qry = $dbconn->query('SELECT * FROM operations');
 
 while($op = $op_qry->fetch_assoc()) {
   $operations[$op['id']] = $op;
 }
 
-if($room_qry = $dbconn->query("SELECT id, room, iteration, product_type, order_status, days_to_ship, room_name, sales_bracket, sample_bracket, preproduction_bracket,
-doordrawer_bracket, main_bracket, custom_bracket, shipping_bracket, install_bracket, pick_materials_bracket, edgebanding_bracket
- FROM rooms WHERE so_parent = '$soID'")) {
+$prev_room = null;
+$prev_sequence = null;
+
+if($room_qry = $dbconn->query("SELECT id, room, iteration, product_type, order_status, days_to_ship, room_name, sales_bracket, sales_published,
+      sample_bracket, preproduction_bracket, preproduction_published, doordrawer_bracket, doordrawer_published, main_bracket, main_published, custom_bracket, 
+      custom_published, shipping_bracket, shipping_published, install_bracket_published, install_bracket, pick_materials_bracket, pick_materials_published, 
+      edgebanding_bracket, edgebanding_published
+    FROM rooms WHERE so_parent = '$soID' ORDER BY room, iteration ASC")) {
   while($room = $room_qry->fetch_assoc()) {
+    $iteration = number_format($room['iteration'], 2);
+
+    //<editor-fold desc="Button Definitions">
+    $edit_btn = "<button class=\"btn waves-effect btn-primary edit_room\" id=\"{$room['id']}\" data-sonum=\"{$so['so_num']}\"><i class=\"zmdi zmdi-edit\"></i></button>";
+    $attachment_btn = "<button class=\"btn waves-effect btn_secondary disabled\" id=\"show_attachments_room_{$room['id']}\"><i class=\"zmdi zmdi-attachment-alt\"></i></button>";
+    $sequence_btn = "<button class=\"btn btn-primary-outline waves-effect add_iteration\" data-roomid=\"{$room['id']}\" data-sonum=\"{$so['so_num']}\" data-addto=\"sequence\" data-iteration=\"{$room['iteration']}\" data-toggle=\"tooltip\" data-placement=\"top\" title=\"\" data-original-title=\"Add additional sequence\" style=\"font-size:10px;width:30px;height:22px;margin-top:1px;padding:0;visibility:visible;\"> S +1</button>";
+    $sequence_btn_hidden = "<button class=\"btn btn-primary-outline waves-effect add_iteration\" data-roomid=\"{$room['id']}\" data-sonum=\"{$so['so_num']}\" data-addto=\"sequence\" data-iteration=\"{$room['iteration']}\" data-toggle=\"tooltip\" data-placement=\"top\" title=\"\" data-original-title=\"Add additional sequence\" style=\"font-size:10px;width:30px;height:22px;margin-top:1px;padding:0;visibility:hidden;\"> S +1</button>";
+    $iteration_btn = "<button class=\"btn btn-primary-outline waves-effect add_iteration\" data-roomid=\"{$room['id']}\" data-sonum=\"{$so['so_num']}\" data-addto=\"iteration\" data-iteration=\"{$room['iteration']}\" data-toggle=\"tooltip\" data-placement=\"top\" title=\"\" data-original-title=\"Add additional iteration\" style=\"font-size:10px;width:30px;height:22px;margin-top:1px;padding:0;visibility:visible;\"> I +.01</button>";
+    $iteration_btn_hidden = "<button class=\"btn btn-primary-outline waves-effect add_iteration\" data-roomid=\"{$room['id']}\" data-sonum=\"{$so['so_num']}\" data-addto=\"iteration\" data-iteration=\"{$room['iteration']}\" data-toggle=\"tooltip\" data-placement=\"top\" title=\"\" data-original-title=\"Add additional iteration\" style=\"font-size:10px;width:30px;height:22px;margin-top:1px;padding:0;visibility:hidden;\"> I +.01</button>";
+    //</editor-fold>
+
+    //<editor-fold desc="Room details">
     $output[$i]['id'] = $room['id'];
     $output[$i]['room'] = $room['room'];
-    $output[$i]['iteration'] = $room['iteration'];
+    $output[$i]['iteration'] = $iteration;
     $output[$i]['product_type'] = $room['product_type'];
-    $output[$i]['order_status'] = $room['order_status'];
+    $output[$i]['order_status'] = $vin_schema['order_status'][$room['order_status']];
     $output[$i]['days_to_ship'] = $room['days_to_ship'];
     $output[$i]['room_name'] = $room['room_name'];
+    //</editor-fold>
 
+    $seq_it = explode('.', $iteration);
 
-    $output[$i]['sales_bracket'] = !empty($operations[$room['sales_bracket']]) ? $operations[$room['sales_bracket']] : '';
-    $output[$i]['sample_bracket'] = !empty($operations[$room['sample_bracket']]) ? $operations[$room['sample_bracket']] : '';
-    $output[$i]['preproduction_bracket'] = !empty($operations[$room['preproduction_bracket']]) ? $operations[$room['preproduction_bracket']] : '';
-    $output[$i]['doordrawer_bracket'] = !empty($operations[$room['doordrawer_bracket']]) ? $operations[$room['doordrawer_bracket']] : '';
-    $output[$i]['main_bracket'] = !empty($operations[$room['main_bracket']]) ? $operations[$room['main_bracket']] : '';
-    $output[$i]['custom_bracket'] = !empty($operations[$room['custom_bracket']]) ? $operations[$room['custom_bracket']] : '';
-    $output[$i]['shipping_bracket'] = !empty($operations[$room['shipping_bracket']]) ? $operations[$room['shipping_bracket']] : '';
-    $output[$i]['install_bracket'] = !empty($operations[$room['install_bracket']]) ? $operations[$room['install_bracket']] : '';
-    $output[$i]['pick_materials_bracket'] = !empty($operations[$room['pick_materials_bracket']]) ? $operations[$room['pick_materials_bracket']] : '';
-    $output[$i]['edgebanding_bracket'] = !empty($operations[$room['edgebanding_bracket']]) ? $operations[$room['edgebanding_bracket']] : '';
+    //<editor-fold desc="Remove duplication of display data and determine buttons to display">
+    if($prev_room !== $room['room']) {
+      $prev_room = $room['room'];
+      $prev_sequence = $seq_it[0];
+      $output[$i]['room_details'] = "{$room['room']}{$room['iteration']}: {$room['room_name']}";
+      $output[$i]['room_actions'] = "$edit_btn $attachment_btn $sequence_btn $iteration_btn";
+    } else {
+      if($prev_sequence !== $seq_it[0]) {
+        $prev_sequence = $seq_it[0];
+        $output[$i]['room_details'] = "&nbsp;&nbsp;{$room['iteration']}: {$room['room_name']}";
+        $output[$i]['room_actions'] = "$edit_btn $attachment_btn $sequence_btn_hidden $iteration_btn";
+      } else {
+        $output[$i]['room_details'] = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;.{$seq_it[1]}: {$room['room_name']}";
+        $output[$i]['room_actions'] = "$edit_btn $attachment_btn $sequence_btn_hidden $iteration_btn_hidden";
+      }
+    }
+    //</editor-fold>
+
+    //<editor-fold desc="Bracket information">
+    $output[$i]['sales_bracket'] = !empty($operations[$room['sales_bracket']]) ? $operations[$room['sales_bracket']] : array('job_title' => 'Unassigned');
+    $output[$i]['sample_bracket'] = !empty($operations[$room['sample_bracket']]) ? $operations[$room['sample_bracket']] : array('job_title' => 'Unassigned');
+    $output[$i]['preproduction_bracket'] = !empty($operations[$room['preproduction_bracket']]) ? $operations[$room['preproduction_bracket']] : array('job_title' => 'Unassigned');
+    $output[$i]['doordrawer_bracket'] = !empty($operations[$room['doordrawer_bracket']]) ? $operations[$room['doordrawer_bracket']] : array('job_title' => 'Unassigned');
+    $output[$i]['main_bracket'] = !empty($operations[$room['main_bracket']]) ? $operations[$room['main_bracket']] : array('job_title' => 'Unassigned');
+    $output[$i]['custom_bracket'] = !empty($operations[$room['custom_bracket']]) ? $operations[$room['custom_bracket']] : array('job_title' => 'Unassigned');
+    $output[$i]['shipping_bracket'] = !empty($operations[$room['shipping_bracket']]) ? $operations[$room['shipping_bracket']] : array('job_title' => 'Unassigned');
+    $output[$i]['install_bracket'] = !empty($operations[$room['install_bracket']]) ? $operations[$room['install_bracket']] : array('job_title' => 'Unassigned');
+    $output[$i]['pick_materials_bracket'] = !empty($operations[$room['pick_materials_bracket']]) ? $operations[$room['pick_materials_bracket']] : array('job_title' => 'Unassigned');
+    $output[$i]['edgebanding_bracket'] = !empty($operations[$room['edgebanding_bracket']]) ? $operations[$room['edgebanding_bracket']] : array('job_title' => 'Unassigned');
+    //</editor-fold>
+
+    //<editor-fold desc="Determining if published or not">
+    $output[$i]['sales_bracket']['published'] = (bool)$room['sales_published'];
+    $output[$i]['sample_bracket']['published'] = (bool)$room['sample_published'];
+    $output[$i]['preproduction_bracket']['published'] = (bool)$room['preproduction_published'];
+    $output[$i]['doordrawer_bracket']['published'] = (bool)$room['doordrawer_published'];
+    $output[$i]['main_bracket']['published'] = (bool)$room['main_published'];
+    $output[$i]['custom_bracket']['published'] = (bool)$room['custom_published'];
+    $output[$i]['shipping_bracket']['published'] = (bool)$room['shipping_published'];
+    $output[$i]['install_bracket']['published'] = (bool)$room['install_bracket_published'];
+    $output[$i]['pick_materials_bracket']['published'] = (bool)$room['pick_materials_published'];
+    $output[$i]['edgebanding_bracket']['published'] = (bool)$room['edgebanding_published'];
+    //</editor-fold>
 
     $i++;
   }
 }
 
-echo json_encode($output);
+if($strip) {
+  echo strip_tags(json_encode($output));
+} else {
+  echo json_encode($output);
+}
--- html/search/search_results.php
+++ html/search/search_results.php
@@ -7,6 +7,16 @@ $find = sanitizeInput($_REQUEST['find']);
 $exact = (bool)sanitizeInput($_REQUEST['exact']);
 $rid = sanitizeInput($_REQUEST['rID']);
 
+$so_qry = $dbconn->query("SELECT * FROM sales_order so 
+  LEFT JOIN dealers d ON so.dealer_code = d.dealer_id
+WHERE so.so_num LIKE '%$find%' OR LOWER(so.dealer_code) LIKE LOWER('%$find%') OR LOWER(so.project_name) LIKE LOWER('%$find%') 
+      OR LOWER(so.project_mgr) LIKE LOWER('%$find%') OR LOWER(so.name_1) LIKE LOWER('%$find%') OR LOWER(so.name_2) LIKE LOWER('%$find%') 
+      OR LOWER(d.dealer_name) LIKE LOWER('%$find%')
+ORDER BY so_num DESC");
+
+if($so_qry->num_rows > 0) {
+  $so = $so_qry->fetch_assoc();
+}
 ?>
 
 <style>
@@ -16,6 +26,8 @@ $rid = sanitizeInput($_REQUEST['rID']);
     font-weight: bold;
     border-bottom: 1px solid #000;
     display: table;
+    background-color: #FFF;
+    z-index: 2;
   }
 
   .search .header .row {
@@ -25,27 +37,48 @@ $rid = sanitizeInput($_REQUEST['rID']);
   .search .header .cell {
     display: table-cell;
   }
-</style>
 
+  table.dataTable tbody td {
+    padding: 2px;
+  }
+
+  .table-row-hover tbody tr:hover {
+    background-color: rgba(128, 128, 128, .25);
+  }
+
+  .col-red {
+    background-color: rgba(255,0,0,.75);
+  }
+
+  .col-gray {
+    background-color: rgba(192,192,192,.75);
+  }
+
+  .col-green {
+    background-color: rgba(0,255,0,.6);
+  }
+</style>
 
 <div class="card-box search">
   <div class="row">
     <div class="col-md-12">
       <button class="btn btn-primary waves-effect waves-light" id="btn_search_to_main"><i class="zmdi zmdi-arrow-left m-r-5"></i> <span>GO BACK</span></button><br><br>
 
-      <div class="header">
+      <div class="header sticky" style="top:85px;">
         <div class="row">
-          <div class="cell">SO: 924</div>
-          <div class="cell">Dealer/Contractor: B23: Jennings</div>
-          <div class="cell">Customer PO/Project Name: Jennings/Fennessy_Black</div>
-          <div class="cell">Project Manager: Jennings</div>
+          <div class="cell">SO: <?php echo $so['so_num']; ?></div>
+          <div class="cell">Dealer/Contractor: <?php echo "{$so['dealer_code']}: {$so['dealer_name']}" ?></div>
+          <div class="cell">Customer PO/Project Name: <?php echo $so['project_name']; ?></div>
+          <div class="cell">Project Manager: <?php echo $so['contact']; ?></div>
         </div>
       </div>
 
-      <table id="room_info" class="display" style="width:100%">
+      <table id="room_info" class="display compact table-row-hover" style="width:100%">
         <thead>
         <tr>
+          <th></th>
           <th>Room</th>
+          <th>Status</th>
           <th>Sales</th>
           <th>Sample</th>
           <th>Pre-production</th>
@@ -59,14 +92,86 @@ $rid = sanitizeInput($_REQUEST['rID']);
         </tr>
         </thead>
       </table>
+
+
     </div>
   </div>
 </div>
 
 <script>
+var searchTable = null;
+
 $(function() {
-  $("#room_info").DataTable({
-    "ajax": "/html/search/ajax/room_list.php"
+  searchTable = $("#room_info").DataTable({
+    ajax: {
+      "url": "/html/search/ajax/room_list.php?so_id=<?php echo $so['so_num']; ?>",
+      "dataSrc": ""
+    },
+    columns: [
+      {data: 'room_actions'},
+      {data: 'room_details'},
+      {data: 'order_status'},
+      {data: 'sales_bracket.job_title'},
+      {data: 'sample_bracket.job_title'},
+      {data: 'preproduction_bracket.job_title'},
+      {data: 'doordrawer_bracket.job_title'},
+      {data: 'main_bracket.job_title'},
+      {data: 'custom_bracket.job_title'},
+      {data: 'shipping_bracket.job_title'},
+      {data: 'install_bracket.job_title'},
+      {data: 'pick_materials_bracket.job_title'},
+      {data: 'edgebanding_bracket.job_title'}
+    ],
+    createdRow: function(row, data, index) {
+      // determines all bracket coloring
+      function checkBrackets() {
+        function checkCol(colName, colNum) { // actual function to determine coloring for each bracket set
+          if(data[colName]['published'] === true) { // if it's published
+            $('td', row).eq(colNum).addClass('col-green'); // mark the column as green
+          }
+        }
+
+        // checks each column
+        checkCol('sales_bracket', 3);
+        checkCol('sample_bracket', 4);
+        checkCol('preproduction_bracket', 5);
+        checkCol('doordrawer_bracket', 6);
+        checkCol('main_bracket', 7);
+        checkCol('custom_bracket', 8);
+        checkCol('shipping_bracket', 9);
+        checkCol('install_bracket', 10);
+        checkCol('pick_materials_bracket', 11);
+        checkCol('edgebanding_bracket', 12);
+      }
+
+      // for the order status, check and color based on that
+      switch(data['order_status']) {
+        case 'Quote': // if quote
+        case 'Production': // or production
+        case 'Inquiry': // or inquiry
+        case 'Pending': // or pending
+          checkBrackets(); // simply color the brackets
+          break;
+        case 'Pillar Missing': // if it's pillar missing
+          $('td', row).addClass('col-red'); // add red to the row
+          checkBrackets(); // then color the brackets
+          break;
+        default: // otherwise we're setting it to gray
+          $('td', row).addClass('col-gray');
+          break;
+      }
+
+      // set the status column to bold
+      $('td', row).eq(2).addClass('font-weight-bold');
+    },
+    paging: false,
+    info: false,
+    searching: false,
+    ordering: false,
+    scrollY: "74.7vh",
+    scrollCollapse: true
   });
+
+
 });
 </script>
