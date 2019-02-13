ca6d7c43681a94efa0c0ea2bdc7195423fb8172b
Ben Beach <subz33r0@gmail.com>
2019-02-13 16:17:27 -0500

    Fixed the multiplier for initial customer creation on an existing customer

--- html/crm/ajax/company.php
+++ html/crm/ajax/company.php
@@ -390,7 +390,7 @@ switch($_REQUEST['action']) {
                               ship_state, ship_zip, ship_country, billing_type, multiplier, payment_method, payment_terms, federal_id, federal_exempt_reason, created) 
                               VALUES ({$info['contactID']}, {$_SESSION['userInfo']['id']}, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, UNIX_TIMESTAMP())");
 
-          $cust_stmt->bind_param('iiiiiiiiiiiiiisisssssiiiisi', $established_date, $info['cust_status'], $info['cust_group'], $info['cust_max_commission'],
+          $cust_stmt->bind_param('iiiiiiiiiiiiiisisssssidiisi', $established_date, $info['cust_status'], $info['cust_group'], $info['cust_max_commission'],
             $info['cust_salesman_commission_user'], $info['cust_salesman_commission'], $info['cust_referral_commission_user'], $info['cust_referral_commission'],
             $info['cust_sales_group_commission_user'], $info['cust_sales_group_commission'], $info['cust_other_commission_user'], $info['cust_other_commission'],
             $info['cust_ship_method'], $info['cust_ship_billto'], $info['cust_ship_account_num'], $info['cust_residential_delivery'],
